cbc8dd4fc5f09ee73e77a87afbc5c96dc1622d03
Alexis-mish <elbergomeztorba424@gmail.com>
2025-05-11 11:35:20 -0700

    Se reconsidero otra clase por la pasada

--- jugs_problem.py
+++ jugs_problem.py
@@ -6,13 +6,9 @@ Problema:
 Write a Python program that, given two jugs of 50 L and 30 L capacity and a user-supplied target volume, prints the shortest sequence of moves to reach that exact volume in either jug—or reports that the target is impossible.
 """
 
-from collections import deque
+class Jarra:
+    def __init__(self, capacidad):
+        self.capacidad = capacidad
 
-class Estado:
-    def __init__(self, jarra1, jarra2, pasos=None, accion="Inicio"):
-        self.jarra1 = jarra1
-        self.jarra2 = jarra2
-        self.accion = accion
-        self.pasos = pasos or []
-    
-    
+if __name__ == "__main__":
+    pass
