31eef9b29e0ec49d52facfc2392348c807fa97cf
Alexis-mish <elbergomeztorba424@gmail.com>
2025-05-11 11:46:59 -0700

    Se reconsidero por la clase estado y se añadió la funcion principal (main)

--- jugs_problem.py
+++ jugs_problem.py
@@ -6,9 +6,20 @@ Problema:
 Write a Python program that, given two jugs of 50 L and 30 L capacity and a user-supplied target volume, prints the shortest sequence of moves to reach that exact volume in either jug—or reports that the target is impossible.
 """
 
-class Jarra:
-    def __init__(self, capacidad):
-        self.capacidad = capacidad
+class Estado:
+    def __init__(self, jarra1, jarra2, pasos=None, accion="Inicio"):
+        self.jarra1 = jarra1
+        self.jarra2 = jarra2
+        self.pasos = pasos or []
+        self.accion = accion
+
+
+def main():
+    # Se pide el objetivo el objetivo en litros
+    try:
+        objetivo = int(input("Ingrese el objetivo en litros: "))
+    except ValueError:
+        print("Error: El objetivo debe ser un número entero")
 
 if __name__ == "__main__":
     pass
